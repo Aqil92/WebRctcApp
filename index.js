62ef1d1f5b06e2be829f6cb52714cd9e79690f62
Mohd Aqil <aqil.mohd.92@gmail.com>
2018-12-29 00:46:58 +0530

    change profile

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var fileServer = new(nodeStatic.Server)();
 var app = https.createServer(options,function(req, res) {
   fileServer.serve(req, res);
 
-}).listen(8080);
+}).listen(process.env.PORT || 5000);
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
