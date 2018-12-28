d17f56e454f270b1ef6a5afce72d446fc96c28fe
Mohd Aqil <aqil.mohd.92@gmail.com>
2018-12-29 01:13:22 +0530

    change profile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var os = require('os');
 var nodeStatic = require('node-static');
-var https = require('httpS');
+var https = require('https');
 var socketIO = require('socket.io');
 var fs = require("fs");
 var options = {
